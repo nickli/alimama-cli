0dd6a6c619ce0e4977956611f0d38104af07ac4a
chongzhi.ldq <chongzhi.ldq@taobao.com>
2016-09-22 21:38:10 +0800

    dev

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var colors = require('colors')
 var program = require('commander')
 var param = process.argv[2]
 var commands = ['init'] //所有命令集合
+var readline = require('readline')
 
 if (!param || commands.indexOf(param) === -1) {
-  console.log('请输入正确的命令（mama init等）'.red)
+  console.error('请输入正确的命令（mama init等）'.red)
   return
 }
+
+
 // param = param || 'init' //默认为init命令
 var command = require('./src/' + param)
 command()
--- src/init.js
+++ src/init.js
@@ -3,34 +3,59 @@
  * @params
  */
 var exec = require('child_process').exec
-var prompt = require('prompt')
+var fs = require('fs')
+  // var prompt = require('prompt')
 var colors = require('colors')
+var readline = require('readline')
 
 module.exports = function() {
   var commandSplit = ';'
 
-  //
-  prompt.start()
-  prompt.get(['projectGit'], function(err, result) {
-
-    var commands = [
-      'git init',
-      'git remote add origin git@gitlab.alibaba-inc.com:thx/scaffold.git',
-      'git pull origin master',
-      'git remote set-url origin ' + result.projectGit,
-      'git push origin master'
-    ]
-    var child = exec(commands.join(commandSplit))
-
-    child.stdout.on('data', function(data) {
-      console.log(data)
-    })
-    child.stderr.on('data', function(data) {
-      console.log(data)
-    })
-    child.on('close', function() {
-      console.log('项目初始化完成'.green)
-    })
-
+  fs.stat('./.git', function(err, stats) {
+    if (stats && stats.isDirectory()) { //已经存在 .git文件夹，说明已经初始化过
+      console.error('项目已经初始化过，请不要重复初始化'.red)
+    } else {
+      execute()
+    }
   })
+
+  var execute = function() {
+    var rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+
+    rl.question('[请输入gitlab上创建好的仓库git地址]'.yellow + ': ', function(answer) {
+
+      if (!answer) {
+        console.error('地址不能为空'.red)
+        rl.close()
+        return
+      }
+
+      var commands = [
+        'git init',
+        'git remote add origin git@gitlab.alibaba-inc.com:thx/scaffold.git',
+        'git pull origin master',
+        'git remote set-url origin ' + answer,
+        'git push origin master'
+      ]
+      var child = exec(commands.join(commandSplit))
+
+      child.stdout.on('data', function(data) {
+        console.log(data)
+      })
+      child.stderr.on('data', function(data) {
+        console.log(data)
+      })
+      child.on('close', function() {
+        console.log('项目初始化完成'.green)
+      })
+
+      rl.close()
+
+    })
+  }
+
+
 }
