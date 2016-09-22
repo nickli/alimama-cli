fe43d7bf76bf7d8cf46c01c7bdf90d7f49923620
chongzhi.ldq <chongzhi.ldq@taobao.com>
2016-09-22 15:33:28 +0800

    dev

--- index.js
+++ index.js
@@ -6,10 +6,3 @@ var cd = exec('gulp publish')
 cd.stdout.on('data', function(data) {
   console.log(data)
 })
-cd.stderr.on('data', function(data) {
-  console.log(data)
-})
-cd.stdin.on('data', function(data) {
-  console.log(data)
-});
-
