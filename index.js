d837dec13399ab4473f6031378aadc5915f85d50
chongzhi.ldq <chongzhi.ldq@taobao.com>
2016-10-14 11:14:22 +0800

    dev

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 var colors = require('colors')
 var program = require('commander')
 var param = process.argv[2]
-var commands = ['init'] //所有命令集合
+var commands = [
+  'init',
+  'dev',
+  'daily',
+  'publish'
+] //所有命令集合
 var readline = require('readline')
 
 if (!param || commands.indexOf(param) === -1) {
