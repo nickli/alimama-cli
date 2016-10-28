28796bb4fce3ce41949cbc2174e2636f7fb79f67
chongzhi.ldq <chongzhi.ldq@taobao.com>
2016-10-28 16:28:08 +0800

    dev

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var colors = require('colors')
 var param = process.argv[2]
+//所有命令集合
 var commands = [
   'init', //项目初始化
   'dev', //运行本地服务器，并在浏览器打开
@@ -8,7 +9,7 @@ var commands = [
   'publish', //cdn发布
   'view', //生成最基础的Magix view, 包含html, js 文件
   'spmlog' //spm埋点
-] //所有命令集合
+]
 
 if (!param || commands.indexOf(param) === -1) {
   console.error('请输入正确的命令（mama init等）'.red)
