2a23e3f18579f1fe50828a1421cf6c8625256f41
chongzhi.ldq <chongzhi.ldq@taobao.com>
2016-11-02 11:49:27 +0800

    dev

--- commands/init.js
+++ commands/init.js
@@ -24,6 +24,15 @@ module.exports = function() {
     fs.writeFileSync(fileName, result, 'utf8')
   }
 
+  //读取index.html，更改daily及cdn的地址
+  let setCdnUrl = function(name) {
+    let fileName = name + '/index.html'
+    let data = fs.readFileSync(fileName, 'utf8')
+    let result = data.replace(/thx\/scaffold/g, 'mm/' + name)
+
+    fs.writeFileSync(fileName, result, 'utf8')
+  }
+
   //读取gulpfile.js更改spmlog的logkey
   let setSpmLogkey = function(logkey, name) {
     let fileName = name + '/gulpfile.js'
@@ -36,64 +45,67 @@ module.exports = function() {
   /*
     根据项目名称创建项目目录，并clone脚手架代码到本地，然后更改git remote为项目git地址
    */
-  rl.question('【请输入项目名称】：'.yellow, function(name) {
-    rl.question('【请输入gitlab上创建好的仓库git地址】：'.yellow, function(gitUrl) {
-      rl.question('【请输入RAP上建好的项目的projectId,非必填】：'.yellow, function(projectId) {
-        rl.question('【请输入埋点用的黄金令箭logkey,非必填】：'.yellow, function(logkey) {
+  rl.question('【请输入gitlab上创建好的仓库git地址】：'.yellow, function(gitUrl) {
+    if (!gitUrl) {
+      console.error('项目地址不能为空'.red)
+      rl.close()
+      return
+    }
 
-          if (!gitUrl) {
-            console.error('项目地址不能为空'.red)
-            rl.close()
-            return
+    let gitUrlMatchResult = gitUrl.match(/^.+\/(.+)\.git\s*$/)
+    let name = gitUrlMatchResult && gitUrlMatchResult[1] || ''
+
+    if (!name) {
+      console.error('项目地址不合法'.red)
+      rl.close()
+      return
+    }
+
+    rl.question('【请输入RAP上建好的项目的projectId,非必填】：'.yellow, function(projectId) {
+      rl.question('【请输入埋点用的黄金令箭logkey,非必填】：'.yellow, function(logkey) {
+
+        let commands = [
+          'mkdir ' + name,
+          'cd ' + name,
+          'git init',
+          'git remote add origin git@gitlab.alibaba-inc.com:thx/scaffold.git',
+          'git pull origin master',
+          'git remote set-url origin ' + gitUrl
+        ]
+
+        //执行clone scaffold脚手架仓库命令
+        util.execCommand(commands).then(function() {
+          setCdnUrl(name)
+
+          if (projectId) {
+            setRapProjectId(projectId, name)
+          }
+          if (logkey) {
+            setSpmLogkey(logkey, name)
           }
 
-          if (!name) {
-            console.error('项目名称不能为空'.red)
-            rl.close()
-            return
-          }
-
-          let commands = [
-            'mkdir ' + name,
-            'cd ' + name,
-            'git init',
-            'git remote add origin git@gitlab.alibaba-inc.com:thx/scaffold.git',
-            'git pull origin master',
-            'git remote set-url origin ' + gitUrl
+          //设置完projectId，logkey之后，提交代码并开始安装npm包
+          let lastCommands = [
+            'cd ' + name, //要进入目录才行
+            'git add . -A',
+            'git commit -m "first commit by alimama-cli"',
+            'git push origin master',
+            'echo 【开始安装项目相关的npm包，请稍等...】'
           ]
 
-          //执行clone scaffold脚手架仓库命令
-          util.execCommand(commands).then(function() {
-            if (projectId) {
-              setRapProjectId(projectId, name)
-            }
-            if (logkey) {
-              setSpmLogkey(logkey, name)
-            }
+          //默认用npm install安装包，可以配置mama init --n=cnpm 来选择cnpm install
+          let npmInstallCommand = 'npm install'
+          if (params.n) {
+            npmInstallCommand = params.n + ' install'
+          }
+          lastCommands.push(npmInstallCommand)
 
-            //设置完projectId，logkey之后，提交代码并开始安装npm包
-            let lastCommands = [
-              'cd ' + name, //要进入目录才行
-              'git add . -A',
-              'git commit -m "first commit by alimama-cli"',
-              'git push origin master',
-              'echo 【开始安装项目相关的npm包，请稍等...】'
-            ]
-
-            //默认用npm install安装包，可以配置mama init --n=cnpm 来选择cnpm install
-            let npmInstallCommand = 'npm install'
-            if (params.n) {
-              npmInstallCommand = params.n + ' install'
-            }
-            lastCommands.push(npmInstallCommand)
-
-            util.execCommand(lastCommands).then(function() {
-              console.log('【项目初始化完成】'.green)
-            })
+          util.execCommand(lastCommands).then(function() {
+            console.log('【项目初始化完成】'.green)
           })
-
-          rl.close()
         })
+
+        rl.close()
       })
     })
   })
